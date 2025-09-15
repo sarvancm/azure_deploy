2930b31e3be5b17c165b94d0cc63c19cd79ca467
sarvancm <cmsarvan202015@gmail.com>
2025-09-15 07:58:29 +0530

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import os
+import platform
+
+app = FastAPI()
+
+@app.get("/")
+async def check_environment():
+    return {
+        "status": "ok",
+        "environment": {
+            "platform": platform.platform(),
+            "python_version": platform.python_version(),
+            "hostname": platform.node(),
+            "env_vars": os.getenv('ENVIRONMENT')
+        }
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
